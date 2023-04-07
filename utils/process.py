a7eb1071046e247bfdaaf0ca56bfcb5bf1cc55e8
mchriscuit <mchriscuit@gmail.com>
2023-04-07 11:54:40 -0400

    Updated preprocessing script to increase readability and to fit new code structure

--- utils/process.py
+++ utils/process.py
@@ -1,8 +1,8 @@
 import numpy as np
 import json
 import argparse
-
 from models.Grammar import Grammar
+
 
 """ *=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=
                     ARGUMENT PARSER
@@ -25,9 +25,27 @@ def parse_args():
         type=str,
         dest="fm",
         help="Filename containing the MCMC parameters",
+        required=True,
     )
     args = parser.parse_args()
     return args
+
+
+""" *=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=
+                    GENERIC LOADER
+=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=* """
+
+
+def jload(filename: str) -> dict:
+    with open(filename) as f:
+        content = json.load(f)
+    return content
+
+
+def aload(filename: str) -> list:
+    with open(filename) as f:
+        content = [c.strip().split(",") for c in f.readlines()]
+    return content
 
 
 """ *=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=
@@ -35,58 +53,54 @@ def parse_args():
 =*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=* """
 
 
-def load(filename):
-    with open(filename) as f:
-        content = json.load(f)
-    return content
-
-
-def load_content(filename):
-    with open(filename) as f:
-        content = [c.strip().split(",") for c in f.readlines()]
-    return content
-
-
-def load_data(filename):
+def process_obs(filename):
     """Loads in the surface data given a filename. Returns the lexemes, lexical
-    items, and number of observations for each lexical item. Due to dimensionality
-    issue, all variables are returned as lists.
+    items, and number of observations for each lexical item.
+
+    (1) filename (str): name of the file to be read
     """
 
     ## Read in the data file
     prefix = "./parameters/data/"
-    content = load_content(f"{prefix}{filename}")
+    content = aload(f"{prefix}{filename}")
+
+    ## Unzip the outputs and number of observations
     *out, nbs = zip(*content)
 
-    ## Sort and split the outputs
-    cxs, srs = zip(*sorted(zip(*out)))
+    ## Sort and split the outputs into lexical items and surface forms
+    cxs, obs = zip(*sorted(zip(*out)))
 
-    ## Generate the clxs by splitting by the delimiter ":"
+    ## Generate the lexical items
     cxs = np.array([tuple(cx.split(":")) for cx in cxs], dtype=object)
 
-    ## Generate the string lxs by getting all the unique lxs
+    ## Generate all the lexemes
     lxs = np.array(sorted(set(lx for cx in cxs for lx in cx)))
 
-    ## Convert the tuple of SRs to a list of SRs
-    srs = np.array(srs)
+    ## Convert the tuple of surface forms to a numpy array
+    obs = np.array(obs)
 
-    ## Convert the tuple of observations to a list of observations
+    ## Convert the tuple of number of observations to a numpy array
     nbs = np.array([float(n) for n in nbs])
 
     ## Create a dictionary to store all of the variables
-    data = {"lxs": lxs, "cxs": cxs, "srs": srs, "nbs": nbs}
+    data = (lxs, cxs, obs, nbs)
 
     return data
 
 
-def load_inventory(filename):
+def process_inv(filename):
     """Loads in and reads an inventory file. Returns the tokens, features, and feature
     configurations for each token as numpy arrays.
+
+    (1) filename (str): name of the file to be read
     """
 
-    ## Read in the inventory
+    ## Read in the inventory file
     prefix = "./parameters/inventory/"
-    content = np.array(load_content(f"{prefix}{filename}"))
+    content = aload(f"{prefix}{filename}")
+
+    ## Convert to numpy array for easy indexing
+    content = np.array(content)
 
     ## Separate the matrix into vectors / matrices for each of the variables
     tokens = content[:, 0][1:]
@@ -94,67 +108,63 @@ def load_inventory(filename):
     configs = content[1:, 1:].astype(int)
 
     ## Create a dictionary to store all of the variables
-    inventory = {"tokens": tokens, "feats": feats, "configs": configs}
+    inventory = (tokens, feats, configs)
 
     return inventory
 
 
-def load_rules(filename):
-    """Loads in rules file and returns a tuple of rule names and definitions"""
+def process_rls(filename):
+    """Loads in rules file. Returns the rule names and definitions in feature notation.
+
+    (1) filename (str): name of the file to be read
+    """
 
     ## Read in the inventory
     prefix = "./parameters/mappings/"
-    content = np.array(load_content(f"{prefix}{filename}"))
+    content = aload(f"{prefix}{filename}")
+
+    ## Convert to numpy array for easy indexing
+    content = np.array(content)
 
     ## Separate the matrix into vectors / matrices for each of the variables
     rnames = content[:, 0]
     rdefs = content[:, 1:]
 
     ## Create a dictionary to store all of the variables
-    rules = {"mnms": rnames, "mdfs": rdefs}
+    rules = (rnames, rdefs)
 
     return rules
 
 
 """ *=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=
-                GRAMMAR INSTANTIATION
+                PARAMETER INSTANTIATION
 =*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=* """
 
 
-def process_grammar(data, inventory, lexicon, mappings):
+def process_parameters(obs, inv, map):
 
     ## Load the data parameters
-    d = load_data(data["fn"])
+    d = process_obs(obs)
+
+    ## Load mappings
+    m = process_rls(map)
 
     ## Load inventory
-    i = load_inventory(inventory["fn"])
+    i = process_inv(inv)
 
-    ## Load the lexicon
-    l = {k: v for k, v in lexicon.items() if k != "fn"}
-
-    ## Load and initialize mappings
-    m = load_rules(mappings["fn"])
-
-    ## Initialize the model
-    G = Grammar(i, l, m, **d)
-
-    return G
+    return d, m, i
 
 
-def initialize():
-    """Loads the parameters filename and populates a ``Grammar``
-    with the provided parameters. Returns the ``Grammar`` object
-    and MCMC parameters for the algorithm
+def load_parameters():
+    """Loads the parameters filename and populates a ``Grammar`` with the provided
+    parameters. Returns the ``Grammar`` object and MCMC parameters for the algorithm
     """
 
     ## Load the arguments from the command-line
     args = parse_args()
 
-    ## Retrieve the GRAMMAR parameters and process the variables
-    gparams = load(args.fp)
-    G = process_grammar(**gparams)
+    ## Load the parameters from the file
+    grP = jload(args.fp)
+    mcP = jload(args.fm)
 
-    ## Retrieve the MCMC parameters the variables
-    C = load(args.fm)
-
-    return G, C, gparams
+    return grP, mcP
