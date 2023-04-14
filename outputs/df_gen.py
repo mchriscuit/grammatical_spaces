75b74e9379f119c20e50e9fbd2e58151dcb2d4b8
mchriscuit <mchriscuit@gmail.com>
2023-04-13 22:51:47 -0400

    Completed dataframe generator

---
+++ outputs/df_gen.py
@@ -0,0 +1,98 @@
+import pandas as pd
+import re
+import json
+import argparse
+
+
+""" *=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=
+            ARGUMENT PARSER
+=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=* """
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-ifp", nargs="+", type=str, required=True, help="")
+    parser.add_argument("-ofp", nargs="?", type=str, required=True, help="")
+    args = parser.parse_args()
+    return args
+
+
+""" *=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=
+            MAIN FUNCTION CALL
+=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=* """
+
+
+def main():
+    ## Retrieve the arguments from the commandline
+    args = parse_args()
+
+    ## Get the filenames of the predictive posteriors
+    ifps = args.ifp
+
+    ## Loop through each of the files; in the directory outside of the folder
+    ## containing the predictive posterior file, we expect there to be a json
+    ## with title ``expected`` with labels corresponding to the expected and
+    ## optionally the alternative outputs for the test items. If no alternatives
+    ## are given, we do not renormalize; if alternatives are given, then we
+    ## renormalized the probabilities based on the sum of the two options
+    dfs = []
+    for fp in ifps:
+        ## Read in the expected file and predictive files
+        pt = f"{re.sub(r'(?<=/)lm.*', '', fp)}{'expected.json'}"
+        with open(pt) as fpt, open(fp) as ffp:
+            jex = json.load(fpt)
+            jdf = json.load(ffp)
+
+        ## Clean the dataframe to eliminate padding
+        jdf = {
+            cx: {re.sub("#", "", f): p for f, p in i.items()} for cx, i in jdf.items()
+        }
+
+        ## Create a dataframe structure to follow the following organization:
+        ## (1) the outermost dictionary corresponds to the lexical item (cx)
+        ## (2) the inner dictionary corresponds to the expected forms and
+        ##     their posterior predictive
+        df = {
+            "cx": [],
+            "trial": [],
+            "int": [],
+            "alt": [],
+            "int_prb": [],
+            "alt_prb": [],
+            "nprb": [],
+        }
+        for cx, info in jex.items():
+            intd = info["int_sr"]
+            altd = info["alt_sr"]
+            trial = info["trial"]
+            pint = jdf[cx].get(intd, 0)
+            palt = jdf[cx].get(altd, 0)
+            pnrm = jdf[cx].get(intd, 0) / (jdf[cx].get(intd, 0) + jdf[cx].get(altd, 0))
+            df["cx"].append(cx)
+            df["trial"].append(trial)
+            df["int"].append(intd)
+            df["alt"].append(altd)
+            df["int_prb"].append(pint)
+            df["alt_prb"].append(palt)
+            df["nprb"].append(pnrm)
+
+        ## Transform to dataframe
+        df = pd.DataFrame(df)
+
+        ## Add language column to dataframe
+        lg = re.sub(r"outputs/(.*?)/lm.*", r"\1", fp)
+        df["lng"] = lg
+
+        ## Append dataframe to list
+        dfs.append(df)
+
+    ## Generate a single dataframe
+    dfs = pd.concat(dfs)
+
+    ## Save dataframe to file
+    ofp = args.ofp
+    dfs.to_csv(ofp)
+
+
+if __name__ == "__main__":
+    main()
